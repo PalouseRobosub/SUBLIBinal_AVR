bfbd4178b4ac6b10b8ab17144e37d765c35b711e
ryan-summers <summers.ryan.m@gmail.com>
2015-12-18 10:41:43 -0800

    Addition of Timer1 and Timer2 functionality
    This commit adds functionality for both timer 1 and timer 2 to the library. Future commits will add in the other timer support functions.

--- src/Timer.c
+++ src/Timer.c
@@ -69,10 +69,116 @@ Error initialize_timer(Timer_Config config) {
 			
 			break;
 		case TIMER_1:
-			return ERR_BAD_CONFIG;
+			//Clear out the configuration
+			TCCR1A = 0;
+			TCCR0B &= ~((1 << CS12)|(1 << CS11)|(1 << CS10)); //clear the clk prescaler bits
+			TIMSK1 = 0;
+			
+			//figure out the prescaler, set the period register
+			if(config.frequency > (float)config.pbclk/((long)65535))
+			{
+				TCCR1B |= (1 << CS10);
+				period = config.pbclk/(config.frequency*1);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)65535*8))
+			{
+				TCCR1B |= (1 << CS11);
+				period = config.pbclk/(config.frequency*8);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)65535*64))
+			{
+				TCCR1B |= (1 << CS11)|(1 << CS10);
+				period = config.pbclk/(config.frequency*64);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)65535*256))
+			{
+				TCCR1B |= (1 << CS12);
+				period = config.pbclk/(config.frequency*256);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)65535*1024))
+			{
+				TCCR1B |= (1 << CS12)|(1 << CS10);
+				period = config.pbclk/(config.frequency*1024);
+			}
+			else
+			{
+				return ERR_BAD_CONFIG;
+			}
+			OCR1A = period;
+			
+			//set mode to CTC (WGM bits are 0100)
+			TCCR0B |= (1 << WGM12);
+			TCCR0B &= ~(1 << WGM13);
+			TCCR0A &= ~(1 << WGM11);
+			TCCR0A &= ~(1 << WGM10);
+
+			if (config.callback != NULL) {
+				//setup interrupts
+				TIMSK1 |= (1 << OCIE1A);
+				timer_1_callback = config.callback;
+			}
+			
 			break;
 		case TIMER_2:
-			return ERR_BAD_CONFIG;
+			//clear previous config
+			TCCR2A = 0;
+			TCCR2B = 0;
+			TIMSK2 = 0;
+			
+			//determine the clock divider and prescaler
+			if(config.frequency > (float)config.pbclk/((long)255))
+			{
+				TCCR2B |= (1 << CS20);
+				period = config.pbclk/(config.frequency*1);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)255*8))
+			{
+				TCCR2B |= (1 << CS21);
+				period = config.pbclk/(config.frequency*8);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)255*32))
+			{
+				TCCR2B |= (1 << CS21)|(1 << CS20);
+				period = config.pbclk/(config.frequency*32);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)255*64))
+			{
+				TCCR2B |= (1 << CS22);
+				period = config.pbclk/(config.frequency*64);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)255*128))
+			{
+				TCCR2B |= (1 << CS22)|(1 << CS20);
+				period = config.pbclk/(config.frequency*128);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)255*256))
+			{
+				TCCR2B |= (1 << CS22)|(1 << CS21);
+				period = config.pbclk/(config.frequency*256);
+			}
+			else if(config.frequency > (float)config.pbclk/((long)255*1024))
+			{
+				TCCR2B |= (1 << CS22)|(1 << CS21)|(1 << CS20);
+				period = config.pbclk/(config.frequency*1024);
+			}
+			else
+			{
+				return ERR_BAD_CONFIG;
+			}
+			
+			OCR2A = period;
+			
+			//set CTC mode (WGM bits 010)
+			TCCR2B &= ~(1 << WGM22);
+			TCCR2A |= (1 << WGM21);
+			TCCR2A &= ~(1 << WGM20);
+			
+			//setup the interrupt mask			
+			if (config.callback != NULL) {
+				//setup interrupts
+				TIMSK2 |= (1 << OCIE2A);
+				timer_2_callback = config.callback;
+			}
 			break;
 		default:
 			//error?
@@ -86,8 +192,24 @@ Error initialize_timer(Timer_Config config) {
 
 ISR(TIMER0_COMPA_vect)
 {
-	if (timer_0_callback != NULL) 
+	if (timer_0_callback != NULL)
 	{
 		timer_0_callback();
+	}
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	if (timer_1_callback != NULL)
+	{
+		timer_1_callback();
+	}
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+	if (timer_2_callback != NULL)
+	{
+		timer_2_callback();
 	}
 }
