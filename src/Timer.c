a0009b4fef5727c0a94c8f0ff2b8127d255bd31b
ryan <summers.ryan.m@gmail.com>
2015-12-26 02:40:24 -0800

    Correction of Timer Enable and Disable
    This commit corrects the timer enable and disable functions by storing the clock dividers
    in global variables. When disabled, the clock div is cleared. When enabled, the clock divider is
    reset to what its previous state was.

--- src/Timer.c
+++ src/Timer.c
@@ -11,6 +11,8 @@ void (*timer_0_callback) (void);
 void (*timer_1_callback) (void);
 void (*timer_2_callback) (void);
 
+volatile char cs_0 = 0, cs_1 = 0, cs_2 = 0;
+
 Error initialize_Timer(Timer_Config config) {
 	Error return_code = ERR_NO_ERR;
 	uint16_t period;
@@ -26,31 +28,31 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR0A = 0;
 				TIMSK0 = 0;
 		
-				TCCR0B &= ~((1 << CS02)|(1 << CS01)|(1 << CS00)); //clear the clk prescaler bits
+				cs_0 &= ~((1 << CS02)|(1 << CS01)|(1 << CS00)); //clear the clk prescaler bits
 				//figure out the prescaler, set the period register
 				if(config.frequency > (float)config.pbclk/((long)255))
 				{
-					TCCR0B |= (1 << CS00);
+					cs_0 |= (1 << CS00);
 					period = config.pbclk/(config.frequency*1);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*8))
 				{
-					TCCR0B |= (1 << CS01);
+					cs_0 |= (1 << CS01);
 					period = config.pbclk/(config.frequency*8);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*64))
 				{
-					TCCR0B |= (1 << CS01)|(1 << CS00);
+					cs_0 |= (1 << CS01)|(1 << CS00);
 					period = config.pbclk/(config.frequency*64);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*256))
 				{
-					TCCR0B |= (1 << CS02);
+					cs_0 |= (1 << CS02);
 					period = config.pbclk/(config.frequency*256);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*1024))
 				{
-					TCCR0B |= (1 << CS02)|(1 << CS00);
+					cs_0 |= (1 << CS02)|(1 << CS00);
 					period = config.pbclk/(config.frequency*1024);
 				}
 				else
@@ -64,6 +66,12 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR0A |= (1 << WGM01);
 				TCCR0A &= ~(1 << WGM00);
 
+				if (config.enabled == TRUE)
+				{
+					TCCR0B &= ~((1<<CS01)|(1<<CS02)|(1<<CS00));
+					TCCR0B |= cs_0;
+				}
+
 				if (config.callback != NULL) {			
 					//setup interrupts
 					TIMSK0 |= (1 << OCIE0A);
@@ -74,33 +82,33 @@ Error initialize_Timer(Timer_Config config) {
 			case TIMER_1:
 				//Clear out the configuration
 				TCCR1A = 0;
-				TCCR1B &= ~((1 << CS12)|(1 << CS11)|(1 << CS10)); //clear the clk prescaler bits
+				cs_1 &= ~((1 << CS12)|(1 << CS11)|(1 << CS10)); //clear the clk prescaler bits
 				TIMSK1 = 0;
 			
 				//figure out the prescaler, set the period register
 				if(config.frequency > (float)config.pbclk/((long)65535))
 				{
-					TCCR1B |= (1 << CS10);
+					cs_1 |= (1 << CS10);
 					period = config.pbclk/(config.frequency*1);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)65535*8))
 				{
-					TCCR1B |= (1 << CS11);
+					cs_1 |= (1 << CS11);
 					period = config.pbclk/(config.frequency*8);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)65535*64))
 				{
-					TCCR1B |= (1 << CS11)|(1 << CS10);
+					cs_1 |= (1 << CS11)|(1 << CS10);
 					period = config.pbclk/(config.frequency*64);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)65535*256))
 				{
-					TCCR1B |= (1 << CS12);
+					cs_1 |= (1 << CS12);
 					period = config.pbclk/(config.frequency*256);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)65535*1024))
 				{
-					TCCR1B |= (1 << CS12)|(1 << CS10);
+					cs_1 |= (1 << CS12)|(1 << CS10);
 					period = config.pbclk/(config.frequency*1024);
 				}
 				else
@@ -115,6 +123,12 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR1A &= ~(1 << WGM11);
 				TCCR1A &= ~(1 << WGM10);
 
+				TCCR1B &= ~((1<<CS10)|(1<<CS11)|(1<<CS12));
+				if (config.enabled == TRUE)
+				{
+					TCCR1B|= cs_1;
+				}
+
 				if (config.callback != NULL) {
 					//setup interrupts
 					TIMSK1 |= (1 << OCIE1A);
@@ -127,41 +141,42 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR2A = 0;
 				TCCR2B = 0;
 				TIMSK2 = 0;
-			
+				cs_2 = 0;
+
 				//determine the clock divider and prescaler
 				if(config.frequency > (float)config.pbclk/((long)255))
 				{
-					TCCR2B |= (1 << CS20);
+					cs_2 |= (1 << CS20);
 					period = config.pbclk/(config.frequency*1);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*8))
 				{
-					TCCR2B |= (1 << CS21);
+					cs_2 |= (1 << CS21);
 					period = config.pbclk/(config.frequency*8);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*32))
 				{
-					TCCR2B |= (1 << CS21)|(1 << CS20);
+					cs_2 |= (1 << CS21)|(1 << CS20);
 					period = config.pbclk/(config.frequency*32);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*64))
 				{
-					TCCR2B |= (1 << CS22);
+					cs_2 |= (1 << CS22);
 					period = config.pbclk/(config.frequency*64);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*128))
 				{
-					TCCR2B |= (1 << CS22)|(1 << CS20);
+					cs_2 |= (1 << CS22)|(1 << CS20);
 					period = config.pbclk/(config.frequency*128);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*256))
 				{
-					TCCR2B |= (1 << CS22)|(1 << CS21);
+					cs_2 |= (1 << CS22)|(1 << CS21);
 					period = config.pbclk/(config.frequency*256);
 				}
 				else if(config.frequency > (float)config.pbclk/((long)255*1024))
 				{
-					TCCR2B |= (1 << CS22)|(1 << CS21)|(1 << CS20);
+					cs_2 |= (1 << CS22)|(1 << CS21)|(1 << CS20);
 					period = config.pbclk/(config.frequency*1024);
 				}
 				else
@@ -176,6 +191,9 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR2A |= (1 << WGM21);
 				TCCR2A &= ~(1 << WGM20);
 			
+				if (config.enabled == TRUE)
+					TCCR2B |= cs_2;
+
 				//setup the interrupt mask			
 				if (config.callback != NULL) {
 					//setup interrupts
@@ -198,6 +216,10 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR0A = 0;
 				TIMSK0 = 0;
 				TCCR0B = 0;
+
+				cs_0 = 0;
+
+
 				//Find the best divider
 				error_1 = config.pbclk/((double)255*1) - config.frequency;
 				if (error_1 < 0)
@@ -214,29 +236,28 @@ Error initialize_Timer(Timer_Config config) {
 				error_1024 = config.pbclk/((double)255*1024) - config.frequency;
 				if (error_1024 < 0)
 					error_1024 *= -1;
+				
 				if (error_1 < error_8)
 					//div is 1
-					TCCR0B |= 1<<CS00;
+					cs_0 |= 1<<CS00;
+				else if (error_8 < error_64)
+					//Div is 8
+					cs_0 |= 1<<CS01;
+				else if (error_64 < error_256)
+					//Div is 64
+					cs_0 |= (1<<CS01)|(1<<CS00);
+				else if (error_256 < error_1024)
+					//Div is 256
+					cs_0 |= 1<<CS02;
 				else
-				{
-					if (error_8 < error_64)
-						//Div is 8
-						TCCR0B |= 1<<CS01;
-					else
-					{
-						if (error_64 < error_256)
-							//Div is 64
-							TCCR0B |= (1<<CS01)|(1<<CS00);
-						else if (error_256 < error_1024)
-							//Div is 256
-							TCCR0B |= 1<<CS02;
-						else
-							//Div is 1024
-							TCCR0B |= (1<<CS02)|(1<<CS00);
-					}
-				}
+					//Div is 1024
+					cs_0 |= (1<<CS02)|(1<<CS00);
+				
+				if (config.enabled == TRUE)
+					TCCR0B |= cs_0;
+			
 				//Set Mode
-					//WGM = 0b000 already done by clearing registers
+				//WGM = 0b000 already done by clearing registers
 				//Set interrupts
 				//setup the interrupt mask
 				if (config.callback != NULL) {
@@ -250,6 +271,9 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR1A = 0;
 				TIMSK1 = 0;
 				TCCR1B = 0;
+
+				cs_1 = 0;
+
 				//Find the best divider
 				error_1 = config.pbclk/((double)65535*1) - config.frequency;
 				if (error_1 < 0)
@@ -272,26 +296,24 @@ Error initialize_Timer(Timer_Config config) {
 					error_1024 *= -1;
 
 				if (error_1 < error_8)
-				//div is 1
-				TCCR1B |= 1<<CS10;
-				else
-				{
-					if (error_8 < error_64)
+					//div is 1
+					cs_1 |= 1<<CS10;
+				else if (error_8 < error_64)
 					//Div is 8
-					TCCR1B |= 1<<CS11;
-					else
-					{
-						if (error_64 < error_256)
-						//Div is 64
-						TCCR1B |= (1<<CS11)|(1<<CS10);
-						else if (error_256 < error_1024)
-						//Div is 256
-						TCCR1B |= 1<<CS12;
-						else
-						//Div is 1024
-						TCCR1B |= (1<<CS12)|(1<<CS10);
-					}
-				}
+					cs_1 |= 1<<CS11;
+				else if (error_64 < error_256)
+					//Div is 64
+					cs_1 |= (1<<CS11)|(1<<CS10);
+				else if (error_256 < error_1024)
+					//Div is 256
+					cs_1 |= 1<<CS12;
+				else
+					//Div is 1024
+					cs_1 |= (1<<CS12)|(1<<CS10);
+								
+				if (config.enabled == TRUE)
+					TCCR1B |= cs_1;
+				
 				//Set Mode
 				//WGM = 0b000 already done by clearing registers
 				//Set interrupts
@@ -307,6 +329,9 @@ Error initialize_Timer(Timer_Config config) {
 				TCCR2A = 0;
 				TIMSK2 = 0;
 				TCCR2B = 0;
+
+				cs_2 = 0;
+
 				//Find the best divider
 				error_1 = config.pbclk/((double)255*1) - config.frequency;
 				if (error_1 < 0)
@@ -329,39 +354,33 @@ Error initialize_Timer(Timer_Config config) {
 				error_1024 = config.pbclk/((double)255*1024) - config.frequency;
 				if (error_1024 < 0)
 					error_1024 *= -1;
+				
 				if (error_1 < error_8)
 					//div is 1
-					TCCR2B |= 1<<CS20;
-				else
-				{
-					if (error_8 < error_32)
+					cs_2 |= 1<<CS20;
+				else if (error_8 < error_32)
 					//Div is 8
-					TCCR2B |= 1<<CS21;
-					else
-					{
-						if (error_32 < error_64)
-							//Div is 32
-							TCCR2B |= (1<<CS21)|(1<<CS20);
-						else 
-						{
-							if (error_64 < error_128)
-								//Div is 64
-								TCCR2B |= 1<<CS22;
-							else
-							{
-								if (error_128 < error_256)
-									//Div 128
-									TCCR2B |= (1<<CS22)|(1<<CS20);
-								else if (error_256 < error_1024)
-									//Div 256
-									TCCR2B |= (1<<CS22)|(1<<CS21);
-								else
-									//Div 1024
-									TCCR2B |= (1<<CS22)|(1<<CS21)|(1<<CS20);
-							}
-						}
-					}
-				}
+					cs_2 |= 1<<CS21;
+				else if (error_32 < error_64)
+					//Div is 32
+					cs_2 |= (1<<CS21)|(1<<CS20);
+				else if (error_64 < error_128)
+					//Div is 64
+					cs_2 |= 1<<CS22;
+				else if (error_128 < error_256)
+					//Div 128
+					cs_2 |= (1<<CS22)|(1<<CS20);
+				else if (error_256 < error_1024)
+					//Div 256
+					cs_2 |= (1<<CS22)|(1<<CS21);
+				else
+					//Div 1024
+					cs_2 |= (1<<CS22)|(1<<CS21)|(1<<CS20);
+
+				if (config.enabled == TRUE)
+					TCCR2B |= cs_2;
+
+
 				//Set Mode
 				//WGM = 0b000 already done by clearing registers
 				//Set interrupts
@@ -387,13 +406,13 @@ Error enable_Timer(Timer_Type which_timer)
 	switch (which_timer)
 	{
 		case TIMER_0:
-			TCCR0A |= 1<<WGM01;
+			TCCR1B |= cs_0;
 			break;
 		case TIMER_1:
-			TCCR1B |= 1<<WGM12;
+			TCCR1B |= cs_1;
 			break;
 		case TIMER_2:
-			TCCR2A |= 1<<WGM21;
+			TCCR2B |= cs_2;
 			break;
 		default: return ERR_INVALID_CHANNEL;
 	}
@@ -405,20 +424,13 @@ Error disable_Timer(Timer_Type which_timer)
 	switch (which_timer)
 	{
 		case TIMER_0:
-			TCCR0A &= ~(1<<WGM00);
-			TCCR0A &= ~(1<<WGM01);
-			TCCR0B &= ~(1<<WGM02);
+			TCCR0B &= ~((1<<CS00)|(1<<CS01)|(1<<CS02));
 			break;
 		case TIMER_1:
-			TCCR1A &= ~(1<<WGM10);
-			TCCR1A &= ~(1<<WGM11);
-			TCCR1B &= ~(1<<WGM12);
-			TCCR1B &= ~(1<<WGM13);
+			TCCR1B &= ~((1<<CS10)|(1<<CS11)|(1<<CS12));
 			break;
 		case TIMER_2:
-			TCCR2A &= ~(1<<WGM20);
-			TCCR2A &= ~(1<<WGM21);
-			TCCR2B &= ~(1<<WGM22);
+			TCCR2B &= ~((1<<CS20)|(1<<CS21)|(1<<CS22));
 			break;
 		default: return ERR_INVALID_CHANNEL;
 	}
